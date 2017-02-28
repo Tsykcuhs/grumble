c9f853ec51464741f5413c0dd73bf3def4586845
tsykcuhs <tsykcuhs@gmail.com>
2017-02-27 21:16:01 -0800

    more changes

--- suffragist.rb
+++ suffragist.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+Choices = {
+	'HAM' => 'Hamburger',
+	'PIZ' => 'Pizza',
+	'CUR' => 'Curry',
+	'NOO' => 'Noodles',
+}
+
 get '/' do
-	'Hello, voter!'
+	@title = 'Welcome to the Suffragist!'
+	erb :index
+end
+
+post '.cast' do
+	@title = 'Thanks for casting your vote!'
+	@vote = params['vote']
+	erb :cast
 end
